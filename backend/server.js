690dc29b2d3e2e122ddac913ec25c60ef15f398f
decaquestionmarks <nateyheit@gmail.com>
2026-06-05 15:48:09 -0700

    0 port

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,6 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, '127.0.0.1', () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running at http://127.0.0.1:${PORT}`);
 });
